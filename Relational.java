ed2fa47e1bef97f628978e36b8b172431c8db8f0
bantupallipavithra <167182970+bantupallipavithra@users.noreply.github.com>
2025-04-28 17:03:38 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,14 @@
+public class Relational{
+public static void main(String[] args){
+int a=43;
+int b=53;
+System.out.println(a);
+System.out.println(b);
+System.out.println(a<b);
+System.out.println(a>b);
+System.out.println(a<=b);
+System.out.println(a>=b);
+System.out.println(a==b);
+System.out.println(a!=b);
+}
+}
